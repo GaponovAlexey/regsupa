82396da155380827961fe7980f7916183b654c69
kekelberi <alexei.gaponov@gmail.com>
2023-02-08 22:33:06 -0600

    save

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,11 @@
 import supabase from "@/supabase"
 import { Inter } from "@next/font/google"
-import { Auth } from "@supabase/ui"
 import Link from "next/link"
-import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  const [users, setUser] = useState("")
-  console.log("users-",users)
-
-  
-  // const r = useRouter()
-  // console.log(r.asPath)
-  const { user, session } = Auth.useUser()
-  console.log("user", user)
-  console.log("session", session)
   const hand = async () => {
     const { data } = supabase.auth.onAuthStateChange()
     console.log("da", data.subscription.id)
@@ -25,18 +14,13 @@ export default function Home() {
 
   useEffect(() => {
     async function getUserData() {
-      await supabase.auth.getUser().then((e) => {
-        console.log("data user", e.data.user)
-        if (e.data?.user) {
-          setUser(e.data?.user)
-        } else {
-
-        }
+      await supabase.auth.getUser().then((v) => {
+        console.log("get", v.data.user.id)
       })
     }
     getUserData()
   }, [])
-  
+
   return (
     <>
       <main className={inter.className}>starts</main>
--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const SUPABASE_URL = "https://iakjucmhukqakhswuqgh.supabase.co"
-const SUPABASE_KEY = process.env.DB_KEY
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 const supabase = createClient(SUPABASE_URL, String(SUPABASE_KEY))
 export default supabase
