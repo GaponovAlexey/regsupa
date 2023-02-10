5fb795275310b5cadb04b5d80da14a5db733ff23
kekelberi <alexei.gaponov@gmail.com>
2023-02-09 22:54:40 -0600

    access token

--- components/Login.js
+++ components/Login.js
@@ -6,7 +6,14 @@ export const LoginApp = () => {
       provider: "google",
     })
   }
+  // const [token, setToken] = useState(null)
+  // const router = useRouter()
 
+  // useEffect(() => {
+  //   const hash = router.asPath
+  //   const newToken = hash.replace('#access_token=', '')
+  //   setToken(newToken)
+  // }, [router.asPath])
   return (
     <div>
       <button onClick={handleLogin}>click</button>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,38 @@
 import supabase from "@/supabase"
 import { Inter } from "@next/font/google"
 import Link from "next/link"
-import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
+
   useEffect(() => {
     supabase.auth.onAuthStateChange((e) => {
       console.log("onAuthStateChange", e)
     })
 
     supabase.auth.getUser().then((v) => {
-      console.log("getUser.id", v.data?.user)
+      console.log("getUser.id", v?.data)
     })
   }, [])
 
   const signOut = async () => {
     await supabase.auth.signOut()
   }
+
+  const [token, setToken] = useState(null)
+  const router = useRouter()
+
+  useEffect(() => {
+    const hash = router.asPath
+    const newToken = hash.split('&')[0].replace('/#access_token=', '')
+    setToken(newToken)
+  }, [router.asPath])
+
+  console.log("token", token)
+  
 
   return (
     <>
