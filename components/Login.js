0ac5a1f8711823eb3dc0f4fc40c0fd61ea0fa36d
kekelberi <alexei.gaponov@gmail.com>
2023-02-09 21:07:37 -0600

    save

--- components/Login.js
+++ components/Login.js
@@ -1,18 +1,12 @@
-import { useEffect } from "react"
 import supabase from "supabase"
 
 export const LoginApp = () => {
   const handleLogin = async () => {
-    const { error: signInError } = await supabase.auth.signInWithOAuth({
+    await supabase.auth.signInWithOAuth({
       provider: "google",
     })
-    console.log(signInError)
   }
-  async function getUserData() {
-    const { da } = await supabase.auth.getUser
-    console.log("da4a", da)
-  }
-  getUserData()
+
   return (
     <div>
       <button onClick={handleLogin}>click</button>
--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,25 @@ import { useEffect } from "react"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  const hand = async () => {
-    const { data } = supabase.auth.onAuthStateChange()
-    console.log("da", data.subscription.id)
-  }
-  hand()
-
   useEffect(() => {
-    async function getUserData() {
-      await supabase.auth.getUser().then((v) => {
-        console.log("get", v.data.user.id)
-      })
-    }
-    getUserData()
+    supabase.auth.onAuthStateChange((e) => {
+      console.log("onAuthStateChange", e)
+    })
+
+    supabase.auth.getUser().then((v) => {
+      console.log("getUser.id", v.data?.user)
+    })
   }, [])
+
+  const signOut = async () => {
+    await supabase.auth.signOut()
+  }
 
   return (
     <>
       <main className={inter.className}>starts</main>
       <Link href="/login">login</Link>
+      <button onClick={signOut}>SignOut</button>
     </>
   )
 }
